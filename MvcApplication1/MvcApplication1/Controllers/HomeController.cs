3460bc30e73135ad3352b11b3ec66db391d239f6
Nikolay <dragg.ko@gmail.com>
2013-02-19 23:55:37 +0300

    begin

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.DataBase;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string Nick, string Password, string ConfinmPassword)
+        {
+            MailEntities mail = new MailEntities();
+            return View();
+        }
+    }
+}
---
+++ MvcApplication1/MvcApplication1/DataBase/ModelMail.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Колян\documents\visual studio 2012\Projects\MvcApplication1\MvcApplication1\DataBase\ModelMail.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
