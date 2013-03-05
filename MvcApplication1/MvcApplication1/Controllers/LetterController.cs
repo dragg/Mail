d7f0b508969afbd0f260f51bc71c3f232b6c183a
Nikolay <dragg.ko@gmail.com>
2013-03-05 21:49:27 +0300

    Новая версия 2.0

--- MvcApplication1/MvcApplication1/Controllers/LetterController.cs
+++ MvcApplication1/MvcApplication1/Controllers/LetterController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using MvcApplication1.DataBase;
+using MvcApplication1.Helper;
 
 namespace MvcApplication1.Controllers
 {
@@ -20,5 +21,25 @@ namespace MvcApplication1.Controllers
             }
         }
 
+        public void Post([FromUri] string val)
+        {
+            
+        }
+
+        public IEnumerable<MvcApplication1.Models.LetterModal> Get([FromUri] string valu, [FromUri] string cookie)
+        {
+            var cookieMas = cookie.Split(';');
+            string login = null;
+            for (int i = 0; i < cookieMas.Length; i++)
+            {
+                var data = cookieMas[i].Split('=');
+                if (data[0] == "Login")
+                {
+                    login = data[1];
+                }
+            }
+
+            return Helper.Helper.GetLetter( valu == "inbox" ? Let.Inbox : Let.Outbox, login).ToArray();
+        }
     }
 }
--- MvcApplication1/MvcApplication1/Scripts/Main.js
+++ MvcApplication1/MvcApplication1/Scripts/Main.js
@@ -5,10 +5,80 @@ function ready()
     $(".Letter").click(function () {
         requestLetter(this.all.Id.value, this);
     });
+    
+    $(".write").click(function()
+    {
+        Write();
+    });
+
+    $('.inbox').click(function () {
+        box('inbox');
+    });
+
+    $('.outbox').click(function () {
+        box('outbox');
+    });
+
+    function box(par)
+    {
+        var jqxhr = $.getJSON("/api/letter?valu=" + par + "&cookie=" + document.cookie);
+        jqxhr.done(function (data) {
+            $("#Letters").remove();
+            $(".Write").remove();
+            var e = $('<div>', { "id": "Letters" });
+            for (var i = 0; i < data.length; i++) {
+                var item = $('<div>', { "class": "Letter" });
+                var dat = 
+                        '<div class="Whom">'+
+                            data[i].UserFromWhom+
+                        '</div>'+
+                        '<div class="Subject">'+
+                            '<a class="getLetter">'+data[i].Subject+'</a>'+
+                        '</div>'+
+                        '<input type="hidden" value='+data[i].Id+' id="Id" name="Id" />';
+                        //item.append(html(dat));
+                        e.append(item.html(dat));
+            }
+            e.appendTo("#message");
+
+            $(".Letter").click(function () {
+                requestLetter(this.all.Id.value, this);
+            });
+        });
+    }
+
+    function Write()
+    {
+        var jqxhr = $.post("/api/letter?val=" + "write", {});
+
+        jqxhr.done(function () {
+            $("#Letters").remove();
+            $(".Write").remove();
+            var e = $('<div>', { "class": "Write" });
+            var data =
+            '<form action="/Home/To_Write" method="post">' +
+                '<p>' +
+                    '<text>Кому:</text>' +
+                    '<input type="text" name="Whom" />' +
+                '</p>' +
+                '<p>' +
+                    '<text>Тема:</text>' +
+                    '<input type="text" name="Subject" />' +
+                '</p>' +
+                '<p>' +
+                    '<text>Текст письма:</text>' +
+                '</p>' +
+                '<textarea name="Text"></textarea>' +
+                '<input id="button" type="submit" value="To Send" />' +
+            '</form>';
+            e.append($("<div>").html(data));
+            e.appendTo($("#message"));
+        });
+    }
 
     function requestLetter(idLetter, where)
     {
-        var jqxhr = $.post("/api/letter?value=" + idLetter, { value: idLetter });
+        var jqxhr = $.post("/api/letter?value=" + idLetter, {});
 
         jqxhr.done(function (data) {
             $('.TextLetter').remove();
